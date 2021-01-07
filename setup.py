d3fc119cbcac72fdd0613de7df36fbc0edd48782
Chris Malek <cmalek@caltech.edu>
2021-01-07 10:05:15 -0800

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ci-buildbot",
-    version='0.7.0',
+    version='0.7.1',
     description="Slack client for reporting on CodePipeline runs",
     url="https://github.com/caltechads/ci-buildbot",
     author="Caltech IMSS ADS",
