201335c245deaeafd8c61521cc3a9efccce64264
Chris Malek <cmalek@caltech.edu>
2024-09-12 13:30:48 -0700

    UPDATE: project: we can now get version and package name from pyproject.toml

--- ci_buildbot/context_processors/project.py
+++ ci_buildbot/context_processors/project.py
@@ -1,7 +1,9 @@
 import pathlib
 import subprocess
 
+import toml
 
+from ..exc import ImproperlyConfiguredError
 from ..typedefs import MessageContext
 from .base import AbstractContextProcessor
 
@@ -19,6 +21,106 @@ class NameVersionProcessor(AbstractContextProcessor):
     for the name and ``make version`` for the version.
     """
 
+    def setup_py(self, path: pathlib.Path) -> dict[str, str]:
+        """
+        Process a setup.py file and return the name and version.
+
+        Raises:
+            ValueError: if the setup.py is a stub, and doesn't
+                contain a version and name
+
+        Args:
+            path: the path to the setup.py file
+
+        Returns:
+            A dictionary with the keys ``name`` and ``version``
+
+        """
+        context: dict[str, str] = {}
+        context["version"] = subprocess.run(
+            ["/usr/bin/env", "python", str(path), "--version"],
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout.strip()
+        if context["version"] == "0.0.0":
+            msg = "setup.py is a stub"
+            raise ValueError(msg)
+        context["name"] = subprocess.run(
+            ["/usr/bin/env", "python", str(path), "--name"],
+            capture_output=True,
+            text=True,
+            check=True,
+        ).stdout.strip()
+        return context
+
+    def makefile(self, path: pathlib.Path) -> dict[str, str]:
+        """
+        Process a Makefile and return the name and version.
+
+        Raises:
+            ValueError: if the Makefile doesn't contain the
+                ``image_name`` or ``version`` targets
+
+        Args:
+            path: the path to the Makefile
+
+        Returns:
+            A dictionary with the keys ``name`` and ``version``
+
+        """
+        context: dict[str, str] = {}
+        # This command line extracts the names of the targets from the Makefile,
+        # ignoring the implicit ones, and sorts them.
+        command = [
+            "make",
+            "-pRrq",
+            "-f",
+            str(path),
+            ":",
+            "2>/dev/null",
+            "|",
+            "awk",
+            "-F':' '/^[a-zA-Z0-9][^$#\t=]*:([^=]|$)/ {print $1}'",
+            "|",
+            "sort",
+            "-u",
+        ]
+        if "image_name" not in subprocess.check_output(command).decode("utf8"):
+            msg = "Makefile does not contain an image_name target"
+            raise ValueError(msg)
+        context["name"] = (
+            subprocess.check_output(["make", "image_name"]).decode("utf8").strip()
+        )
+        context["version"] = (
+            subprocess.check_output(["make", "version"]).decode("utf8").strip()
+        )
+        return context
+
+    def pyproject_toml(self, path: pathlib.Path) -> dict[str, str]:
+        """
+        Process a pyproject.toml file and return the name and version.
+
+        Raises:
+            ValueError: if the pyproject.toml is a stub, and doesn't
+                contain a version and name
+
+        Args:
+            path: the path to the pyproject.toml file
+
+        Returns:
+            A dictionary with the keys ``name`` and ``version``
+
+        """
+        context: dict[str, str] = {}
+        data = toml.load(path)
+        if "project" not in data:
+            msg = "pyproject.toml is a stub: no project section"
+            raise ValueError(msg)
+        context["name"] = data["project"]["name"]
+        context["version"] = data["project"]["version"]
+        return context
+
     def annotate(self, context: MessageContext) -> None:
         """
         Add the following keys to ``context``:
@@ -34,25 +136,32 @@ class NameVersionProcessor(AbstractContextProcessor):
 
         Args:
             context: the current message context
+
         """
         super().annotate(context)
-        setup_py = pathlib.Path.cwd() / 'setup.py'
+        setup_py = pathlib.Path.cwd() / "setup.py"
+        makefile = pathlib.Path.cwd() / "Makefile"
+        pyproject_toml = pathlib.Path.cwd() / "pyproject.toml"
         if setup_py.exists():
-            context['version'] = subprocess.run(
-                ['python', str(setup_py), '--version'],
-                capture_output=True,
-                text=True,
-                check=True
-            ).stdout.strip()
-            context['name'] = subprocess.run(
-                ['python', str(setup_py), '--name'],
-                capture_output=True,
-                text=True,
-                check=True
-            ).stdout.strip()
-        else:
-            # No setup.py; let's try Makefile
-            makefile = pathlib.Path.cwd() / 'Makefile'
-            if makefile.exists():
-                context['name'] = subprocess.check_output(['make', 'image_name']).decode('utf8').strip()
-                context['version'] = subprocess.check_output(['make', 'version']).decode('utf8').strip()
+            try:
+                context.update(self.setup_py(setup_py))
+            except ValueError:
+                pass
+            else:
+                return context
+        if makefile.exists():
+            try:
+                context.update(self.makefile(makefile))
+            except ValueError:
+                pass
+            else:
+                return context
+        if pyproject_toml.exists():
+            try:
+                context.update(self.pyproject_toml(pyproject_toml))
+            except ValueError:
+                pass
+            else:
+                return context
+        msg = "Cannot determine project name and version"
+        raise ImproperlyConfiguredError(msg)
